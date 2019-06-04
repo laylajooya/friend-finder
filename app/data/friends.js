c2f78cd89bdd1d370b55012e3d6026a98cfc7331
Layla Jooya <laylajooya@gmail.com>
2019-06-04 08:35:07 -0500

    js added to survey html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var friends = [
     {
         name: "Layla",
-        photo: "",
-        scores: [1,1,4,1,1,3,2,3,1,4]
+        photo: "https://i.imgur.com/fMo86mo.jpg",
+        scores: [1,5,3,5,5,3,4,2,5,2]
     },
     {
         name: "Aine",
-        photo: "",
-        scores: ""
+        photo: "https://i.imgur.com/ZCI9dr2.jpg",
+        scores: [1,3,4,4,2,2,3,3,3,5]
     },
     {
         name: "Alan",
-        photo: "",
-        scores: ""
+        photo: "https://i.imgur.com/8h29bEH.jpg",
+        scores: [1,4,5,3,2,5,4,4,3,2]
     },
     {
         name: "Mo",
-        photo: "",
-        scores: ""
+        photo: "https://i.imgur.com/k2a9ASV.jpg",
+        scores: [1,5,5,4,2,4,3,4,3,3]
     }
 
 ];
